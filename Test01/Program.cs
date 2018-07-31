efd7cf1e831cfccc31488703a5c910ef13c5a204
AlexandrePoirot <volty0@orange.fr>
2018-07-31 12:13:40 +0200

    Option

--- Test01/Program.cs
+++ Test01/Program.cs
@@ -8,40 +8,70 @@ namespace Test01
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            String j = "";
-            Console.WriteLine("Qu'est ce que tu aimes?");
-            j = Console.ReadLine();
-            Console.WriteLine("Tu aime " + j);
-
-            Console.WriteLine("Et combien en veux tu?");
-            string c = "";
-            c = Console.ReadLine();
-            Console.WriteLine("Tu veux " + c + " " + j +".\n");
-            int d = int.Parse(c);
-            int e = 0;
-            while(e != d)
+            Console.WriteLine("Bienvenue!!!\n--1 Addition\n--2 Chose que tu aimes");
+            string option = "d";
+            option = Console.ReadLine();
+            if (option == "1")
             {
-                Console.WriteLine("Appuie sur n'importe quelle touche pour augmenter le nombre de " + j+"\n");
-                e++;
-                Console.ReadKey();
-                Console.WriteLine(e + " " + j+"\n");
-            };
-
-            Console.WriteLine("Veux tu vendre ton âme pour " + c + " " + j +" ? \nY ou N");
-            c = Console.ReadLine();
-            if(c == "Y" | c == "y")
-            {
-                Console.WriteLine("Merci pour ton âme...dors bien pour ta dernière nuit");
+                Console.WriteLine("Quel deux chiffres veut tu additionner? \n");
+                int calA, calB;
+                calA = int.Parse(Console.ReadLine());
+                calB = int.Parse(Console.ReadLine());
+                Console.WriteLine(calA + " + " + calB + " = " + additionBis(calA, calB));
             }
-            else if(c == "N" | c == "n")
-            {
-                Console.WriteLine("Tant pis, une prochaine fois peut-être...");
+            else if(option == "2")
+            { 
+                String j = "";
+                Console.WriteLine("Qu'est ce que tu aimes?");
+                j = Console.ReadLine();
+                Console.WriteLine("Tu aime " + j);
+
+                Console.WriteLine("Et combien en veux tu?");
+                string c = "";
+                c = Console.ReadLine();
+                Console.WriteLine("Tu veux " + c + " " + j + ".\n");
+                int d = int.Parse(c);
+                int e = 0;
+                while (e != d)
+                    {
+                    Console.WriteLine("Appuie sur n'importe quelle touche pour augmenter le nombre de " + j + "\n");
+                    e++;
+                    Console.ReadKey();
+                    Console.WriteLine(e + " " + j + "\n");
+                    };
+
+                Console.WriteLine("Veux tu vendre ton âme pour " + c + " " + j + " ? \nY ou N");
+                c = Console.ReadLine();
+                if (c == "Y" | c == "y")
+                    {
+                     Console.WriteLine("Merci pour ton âme...dors bien pour ta dernière nuit");
+                    }
+                else if (c == "N" | c == "n")
+                    {
+                     Console.WriteLine("Tant pis, une prochaine fois peut-être...");
+                    }
+                else
+                    {
+                     Console.WriteLine("Tu n'est même pas capable de lire correctement, ton âme n'aurais aucun goût");
+                    }
             }
             else
             {
-                Console.WriteLine("Tu n'est même pas capable de lire correctement, ton âme n'aurais aucun goût");
+                Console.WriteLine("L'option n'est pas encore intégrée");
+            }
+
+            int additionBis(int a, int b)
+            {
+                int cc = 0;
+                int ab = 0;
+                int bb = 0;
+                ab = a;
+                bb = b;
+                cc = ab + bb;
+                return (cc);
             }
         }
     }
